57b574bd13aa850bee609bb277a9fa424b36c525
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-17 09:57:49 +0530

    Fixed bugs and updated code

--- src/Examples/AverageOfLevelsInBinaryTree.java
+++ src/Examples/AverageOfLevelsInBinaryTree.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class AverageOfLevelsInBinaryTree { // Class for average of levels
+public class AverageOfLevelsInBinaryTree {
 
     static class TreeNode { // Simple node structure
         int val; // Node value

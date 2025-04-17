fd974142f3de262c399c43891d2336ccfc496ca8
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-17 10:08:04 +0530

    Fixed bugs and updated code

--- src/Examples/SymmetricTree.java
+++ src/Examples/SymmetricTree.java
@@ -1,7 +1,7 @@
-package Examples; // Organizes class in package
+package Examples;
 
-import java.util.LinkedList; // For queue
-import java.util.Queue; // For Queue type
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class SymmetricTree { // Class for checking symmetric tree
 

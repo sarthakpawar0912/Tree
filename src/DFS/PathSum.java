1d482dc06e72c6be5ad11b5c542e9f57978e8090
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-19 10:06:11 +0530

    Fixed bugs and updated code

--- src/DFS/PathSum.java
+++ src/DFS/PathSum.java
@@ -10,7 +10,7 @@ public class PathSum { // Class for checking path sum
 
         TreeNode() {}
 
-        TreeNode(int val) { // Constructor with value
+        TreeNode(int val) {
             this.val = val;
         }
 

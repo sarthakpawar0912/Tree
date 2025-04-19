37365b0801bbe0c6f483ca3962ebd87c8a09beee
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-19 10:05:17 +0530

    Fixed bugs and updated code

--- src/DFS/PathSum.java
+++ src/DFS/PathSum.java
@@ -2,7 +2,7 @@ package DFS;
 
 public class PathSum { // Class for checking path sum
 
-    // Definition for a binary tree node
+
     public class TreeNode { // Simple node structure (non-static as provided)
         int val; // Node value
         TreeNode left; // Left child

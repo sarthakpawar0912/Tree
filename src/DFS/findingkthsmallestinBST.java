2d5bdbac3d68e78aed2cd422745ae478cc44d407
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-20 23:19:36 +0530

    Fixed bugs and updated code

--- src/DFS/findingkthsmallestinBST.java
+++ src/DFS/findingkthsmallestinBST.java
@@ -2,7 +2,7 @@ package DFS;
 
 public class findingkthsmallestinBST { // Class for finding kth smallest in BST
 
-    // Definition for a binary tree node
+
     public class TreeNode { // Simple node structure (non-static as provided)
         int val; // Node value
         TreeNode left; // Left child

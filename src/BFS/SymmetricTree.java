42d34a7ca4dbbc8cadfc14ff79c00d25445c88c2
sarthakpawar0912 <pawarsr06@gmail.com>
2025-05-05 09:15:28 +0530

    Fixed bugs and updated code

--- src/BFS/SymmetricTree.java
+++ src/BFS/SymmetricTree.java
@@ -5,7 +5,7 @@ import java.util.Queue;
 
 public class SymmetricTree {
 
-    // Definition for a binary tree node
+
     static class TreeNode { // Simple node structure
         int val; // Node value
         TreeNode left; // Left child
